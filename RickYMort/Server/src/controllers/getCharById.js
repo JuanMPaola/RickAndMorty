68a8f86dac79d5f53dd4c58a05222dbb58bd198b
JuanMPaola <juanmpaola25@gmail.com>
2023-09-07 23:55:54 -0300

    Homework promises II

--- RickYMort/Server/src/controllers/getCharById.js
+++ RickYMort/Server/src/controllers/getCharById.js
@@ -1,6 +1,17 @@
 const axios = require('axios')
 
-const getCharById = function (res, id){
-   axios(`https://rickandmortyapi.com/api/character/${id}`)
-   .then()
+
+const getCharById = (res, id) => {
+   axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((response) => {
+         const { id, name, image, gender, species, status, origin } = response.data;
+         res.writeHead((200), { "Content-type": "text/plain" })
+            .end(JSON.stringify({ id, name, image, gender, species, status, origin }))
+      })
+      .catch((error) => res.writeHead(500, { "Content-type": "text/plain" })
+         .end(error.message))
+}
+
+module.exports = {
+   getCharById
 }
--- RickYMort/Server/src/index.js
+++ RickYMort/Server/src/index.js
@@ -1,20 +1,13 @@
-const http = require('http')
-const data = require('./utils/data')
+const http = require("http");
+const {getCharById} = require("./controllers/getCharById")
 
-
-
-http
-.createServer((req, res)=>{
+http.createServer((req, res) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     if(req.url.includes("/rickandmorty/character")){
-        const id = req.url.split('/').at(-1)
-
-        const personajes = data.find((chara)=> chara.id === +id)
-
-        res.writeHead(200, {"Content-Type":"application/json"})
-        return res.end(JSON.stringify(personajes))
+        const id = req.url.split("/").pop()
+        
+        getCharById(res, Number(id))
     }
-
-})
-.listen(3001, 'localhost' )
+    
+}).listen(3001, "localhost");
